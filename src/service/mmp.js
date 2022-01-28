4fd8f13b825f4c17274febcc83e60b09a2437b00
wentai.zhou <943990462@qq.com>
2022-01-28 18:29:01 +0800

    Feature # 改地址，baseURL暂时不动

--- src/service/mmp.js
+++ src/service/mmp.js
@@ -31,7 +31,7 @@ export const apiConfig = {
     v3Api: "/api/v1/cqc/web/file",
     protal:'/api/v1/cqc/portal/sysDict',
     v5Api:'/api/v1/cqc/web/item',
-    productionUrl: "http://54.251.197.76:8081",
+    productionUrl: "http://13.212.197.232:8081",
     isMock: false,
     token:''
 };
